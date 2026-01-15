8c95322183441494eb7041d4038f12bcdd906e8c
Benjamin Aronsson <62105998+AI-nsley69@users.noreply.github.com>
2026-01-15 22:59:27 +0100

    Refactor backup script for retention management
    Updated backup retention logic to use a variable for retention hours and improved backup deletion process.

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 backup_dir="$HOME/backups"
+retention_hours="24"
 
 mkdir -p "$backup_dir"
 
 servers="smp cmp mirror velocity"
 for server in $servers; do
     echo "Backing up $server.."
-    
+
     # Announce that the backup job is starting, incase server perf degrades
     msg=$(jq -cnR '. = { "text": "\u00a75[Hourly Backup] \u00a7dStarting job", "bold": true }')
     echo "tellraw @a $msg" > /run/$server.stdin
-    SECONDS=0 
+    SECONDS=0
     # Create backup dir incase it doesn't exist
     mkdir -p "$backup_dir/$server"
     # Create the archive but exclude the entire bluemap directory, since it's quite large and mostly a dupe of the world
     archive=$(date -u +%Y-%m-%dT%H:%M:%S)
     tar cfz "$backup_dir/$server/$archive.tar.zst" -C "$HOME/$server" --exclude "./bluemap" .
-    
+
     # Announce once the world is backed up
-    msg=$(jq -cnR '. = { "text": "\u00a75[Hourly Backup] \u00a7dFinished job in $s seconds", "bold": true }' --arg s "$seconds")
+    msg=$(jq -cnR '. = { "text": "\u00a75[Hourly Backup] \u00a7dFinished job in \($s) seconds", "bold": true }' --arg s "$seconds")
     echo "tellraw @a $msg" > /run/$server.stdin
-    
+
     echo "Finished backing up $server.."
     # Delete old backups
-    total_backups=$(find "$backup_dir/$server/*" | wc -l)
-    if [ $total_backups -gt 48 ]; then
-        backups_to_delete=$(ls -Art "$backup_dir/$server" | head -n $(( $total_backups - 48 )))
-        echo "Found $(( $total_backups - 48 )) backups to delete.."
-        for backup in $backups_to_delete; do
-            rm "$backup_dir/$server/$backup"
-        done
+    current_dir="$PWD"
+    cd $backup_dir/$server
+    total_backups=$(ls | wc -l)
+    if [ $total_backups -gt $retention_hours ]; then
+        backups_to_delete=$(ls -Art | head -n $(( $total_backups - $retention_hours )))
+        echo "Found $(( $total_backups - $retention_hours )) backups to delete.."
+        rm $backups_to_delete
     fi
+    cd $current_dir
 done
